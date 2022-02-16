56ddfc2b911fa3ae1fbee4768d0c6688913e4652
Luis Abraham Tlapa Garcia <l.tlapa@ssaver.gob.mx>
2022-02-16 17:59:40 -0600

    WIP - video 315

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {  AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -6,16 +6,37 @@ import * as mapboxgl from 'mapbox-gl';
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.component.css']
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!:ElementRef
+  mapa!: mapboxgl.Map
+  zoomLevel: number =10
 
-  constructor() { }
+  constructor() {
+   }
 
-  ngOnInit(): void {
-    const map = new mapboxgl.Map({
-      container: 'mapa', // container ID
+  ngAfterViewInit(): void {
+
+    console.log("after",this.divMapa);
+    
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
       center: [-96.91348207666158, 19.54216578647094], // starting position [lng, lat]
-      zoom: 15 // starting zoom
-  })
+      zoom: this.zoomLevel // starting zoom
+    })
+
+    this.mapa.on('zoom', (ev) => {
+      this.zoomLevel = this.mapa.getZoom()
+      
+      
+    })
+  }
+  zoomIn(){
+    this.mapa.zoomIn()
+  }
+  zomOut(){
+    this.mapa.zoomOut()
+
+    
   }
 }
